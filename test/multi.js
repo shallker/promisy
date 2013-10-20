5da8f18e93185e9606a5fa1c21413d13077697f6
Shallker Wang <shallker.wang@profero.com>
2013-10-20 19:20:23 +0800

    Add test for multi

---
+++ test/multi.js
@@ -0,0 +1,29 @@
+var Promisy = require('../index');
+
+var promisy1 = new Promisy;
+var promisy2 = new Promisy;
+
+promisy1(function (next) {
+  console.log('promisy1 first');
+  next();
+}).then(function (next) {
+  console.log('promisy1 then 1');
+  setTimeout(function () {
+    next();
+  }, 1000);
+}).then(function () {
+  console.log('promisy1 then 2');
+});
+
+promisy2(function (next) {
+  console.log('promisy2 first');
+  next();
+}).then(function (next) {
+  console.log('promisy2 then 1');
+  setTimeout(function () {
+    next();
+  }, 1000);
+}).then(function () {
+  console.log('promisy2 then 2');
+});
+
