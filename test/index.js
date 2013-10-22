22f01315738ba38a1e082bf30035a90a47c0acf6
Shallker Wang <shallker.wang@profero.com>
2013-10-23 01:16:06 +0800

    Using simple-test in tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,23 @@
 var Promisy = typeof document === 'object' ? require('promisy') : require('../lib/promisy');
+var test = require('simple-test');
 var promisy = new Promisy;
 
-promisy(function (next) {
-  console.log('first');
-  next();
-}).then(function (next) {
-  console.log('then 1');
-  setTimeout(function () {
+test('promisy', function (done) {
+  promisy(function (next) {
+    console.log('first');
     next();
-  }, 1000);
-}).then(function (next) {
-  console.log('then 2');
-  setTimeout(function () {
-    next();
-  }, 1000);
-}).then(function (next) {
-  console.log('then 3');
+  }).then(function (next) {
+    console.log('then 1');
+    setTimeout(function () {
+      next();
+    }, 1000);
+  }).then(function (next) {
+    console.log('then 2');
+    setTimeout(function () {
+      next();
+    }, 1000);
+  }).then(function (next) {
+    console.log('then 3');
+    done();
+  });
 });
--- test/multi.js
+++ test/multi.js
@@ -1,29 +1,35 @@
 var Promisy = typeof document === 'object' ? require('promisy') : require('../lib/promisy');
+var test = require('simple-test');
 
 var promisy1 = new Promisy;
 var promisy2 = new Promisy;
 
-promisy1(function (next) {
-  console.log('promisy1 first');
-  next();
-}).then(function (next) {
-  console.log('promisy1 then 1');
-  setTimeout(function () {
+test('promisy one', function (done) {
+  promisy1(function (next) {
+    console.log('promisy1 first');
     next();
-  }, 1000);
-}).then(function () {
-  console.log('promisy1 then 2');
+  }).then(function (next) {
+    console.log('promisy1 then 1');
+    setTimeout(function () {
+      next();
+    }, 500);
+  }).then(function () {
+    console.log('promisy1 then 2');
+    done();
+  });
 });
 
-promisy2(function (next) {
-  console.log('promisy2 first');
-  next();
-}).then(function (next) {
-  console.log('promisy2 then 1');
-  setTimeout(function () {
+test('promisy two', function (done) {
+  promisy2(function (next) {
+    console.log('promisy2 first');
     next();
-  }, 1000);
-}).then(function () {
-  console.log('promisy2 then 2');
+  }).then(function (next) {
+    console.log('promisy2 then 1');
+    setTimeout(function () {
+      next();
+    }, 500);
+  }).then(function () {
+    console.log('promisy2 then 2');
+    done();
+  });
 });
-
